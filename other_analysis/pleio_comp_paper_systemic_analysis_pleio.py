46ab8a35b2a852037dac2f5341e6b473ba0e386b
ali4lou <alicia.lougra@gmail.com>
2024-12-23 17:58:15 +0100

    Update pleio_comp_paper_systemic_analysis_pleio.py

--- other_analysis/pleio_comp_paper_systemic_analysis_pleio.py
+++ other_analysis/pleio_comp_paper_systemic_analysis_pleio.py
@@ -1,11 +1,8 @@
-# -*- coding: utf-8 -*-
+
 """
-Created on Wed Dec 18 14:41:25 2024
 
 Comparison of pleiotropy with paper: Systematic Anlaysis of Pleiotropy in C. elegans 
 
-
-@author: logslab
 """
 
 import numpy as np
@@ -21,15 +18,19 @@ from scipy.stats import fisher_exact
 from sklearn.metrics.pairwise import pairwise_distances
 
 
-path_save_data='C:\\Users\\logslab\\Desktop\\D-P_rule_paper\\pleio_comparison\\pleio_comp_systematic_analysis_pleio\\'
+"""
+path_save_data, path_dev, path_phen, path_sim and path_pleio
+are the path that you chose after download the needed files
+"""
 
+path_save_data='YOUR_PATH_TO_SAVE_DATA'
 
 #1.) We are going to read all the data
-path_dev='C:\\Users\\logslab\\Desktop\\D-P_rule_paper\\matrix_construction\\dev_space\\'
-path_phen='C:\\Users\\logslab\\Desktop\\D-P_rule_paper\\matrix_construction\\phen_space\\'
-path_pleio='C:\\Users\\logslab\\Desktop\\D-P_rule_paper\\NNMF_justification\\'
-path_pleio_genes='C:\\Users\\logslab\\Desktop\\D-P_rule_paper\\section2\\'
-path_deviations='C:\\Users\\logslab\\Desktop\\D-P_rule_paper\\section1_deviations\\'
+path_dev='PATH_WHERE_IS_DOWNLOADED_THE_DATA'
+path_phen='PATH_WHERE_IS_DOWNLOADED_THE_DATA'
+path_pleio_genes='PATH_WHERE_IS_DOWNLOADED_THE_DATA'
+path_pleio='PATH_WHERE_IS_DOWNLOADED_THE_DATA'
+path_deviations='PATH_WHERE_IS_DOWNLOADED_THE_DATA'
 
 
 #1.1.) We read commmon genes
